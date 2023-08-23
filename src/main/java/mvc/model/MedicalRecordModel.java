02f0ec94fafff8b0562d3ad0d98b4777947f00e8
d.krivoruchko <krivoruchkodmitry@gmail.com>
2023-08-23 12:25:24 +0200

    To-Do persist()

--- src/main/java/mvc/model/MedicalRecordModel.java
+++ src/main/java/mvc/model/MedicalRecordModel.java
@@ -15,11 +15,16 @@ public class MedicalRecordModel {
     private MedicalRecordType type;
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private final PatientModel patient;
+
 
     public MedicalRecordModel(LocalDate date, MedicalRecordType type, String description, PatientModel patient) {
         this.date = date;
         this.type = type;
         this.description = description;
+        this.patient = patient;
     }
 
 
